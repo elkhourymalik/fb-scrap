38a0079fc805ed196df6773e2a0ba8792f61b254
malik el khoury <elkhourymalik@gmail.com>
2016-02-04 18:42:45 +0100

    removed the ugly croped

--- js/fb.js
+++ js/fb.js
@@ -8,6 +8,21 @@ $(function() {
       function(data) {
         console.log(data)
     });
+
+    $.get("https://graph.facebook.com/me",
+      { access_token: accessToken },
+      function(data) {
+        console.log(data);
+        var firstName = data.first_name;
+        var lastName = data.last_name;
+
+        var pictureUrl = "http://graph.facebook.com/" + data.id + "/picture?type=square";
+        var profilePic = $("<img>").attr("src", pictureUrl);
+
+        $("#my-profile .basic").append(profilePic);
+        $("#my-profile .basic").append(firstName + ", " + lastName);
+      });
+
     $.get("https://graph.facebook.com/me/photos?fields=album&limit=1000",
       { access_token: accessToken },
     function(data) {
@@ -16,7 +31,7 @@ $(function() {
         console.log(value.id);
         var id = value.id;
         var pictureUrl = "https://graph.facebook.com/" + value.id + "/picture?type=normal&access_token=" + accessToken;
-        var profilePic = $("<img>").attr("src", pictureUrl);
+        var profilePic = $("<img>").attr("src", pictureUrl).addClass("images_found");
         $("#my-profile .advanced").append(profilePic);
       });
     });
